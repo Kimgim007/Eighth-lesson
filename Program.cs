ba8aa67b40bcc1b0f8b03085761e1b385e6192df
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-04-15 20:27:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Eighth_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
